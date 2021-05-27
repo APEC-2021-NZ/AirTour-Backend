0b6cd040f3f53a8ffd79c20416d44fd4f261348a
ZHENK <chengzhenyang@gmail.com>
2021-05-27 18:48:18 +1200

    comment out loadData

--- src/index.js
+++ src/index.js
@@ -17,72 +17,72 @@ admin.initializeApp({
     storageBucket: 'gs://apec-2021-nz.appspot.com',
 })
 
-async function loadData() {
-    let country = Country.init()
-    country.id = '1'
-    country.name = 'New Zealand'
-    await country.save()
+// async function loadData() {
+//     let country = Country.init()
+//     country.id = '1'
+//     country.name = 'New Zealand'
+//     await country.save()
 
-    let city = City.init()
-    city.id = '1'
-    city.name = 'Auckland'
-    city.country = country.key
-    await city.save()
+//     let city = City.init()
+//     city.id = '1'
+//     city.name = 'Auckland'
+//     city.country = country.key
+//     await city.save()
 
-    let city2 = City.init()
-    city2.id = '2'
-    city2.name = 'Wellington'
-    city2.country = country.key
-    await city2.save()
+//     let city2 = City.init()
+//     city2.id = '2'
+//     city2.name = 'Wellington'
+//     city2.country = country.key
+//     await city2.save()
 
-    let language = Language.init()
-    language.id = '1'
-    language.name = 'English'
-    await language.save()
+//     let language = Language.init()
+//     language.id = '1'
+//     language.name = 'English'
+//     await language.save()
 
-    let language2 = Language.init()
-    language2.id = '2'
-    language2.name = 'Not English'
-    await language2.save()
+//     let language2 = Language.init()
+//     language2.id = '2'
+//     language2.name = 'Not English'
+//     await language2.save()
 
-    let experience = Experience.init()
-    experience.id = '1'
-    experience.name = 'Skiing'
-    experience.image = 'url'
-    await experience.save()
+//     let experience = Experience.init()
+//     experience.id = '1'
+//     experience.name = 'Skiing'
+//     experience.image = 'url'
+//     await experience.save()
 
-    let experience2 = Experience.init()
-    experience2.id = '2'
-    experience2.name = 'Biking'
-    experience2.image = 'url'
-    await experience2.save()
+//     let experience2 = Experience.init()
+//     experience2.id = '2'
+//     experience2.name = 'Biking'
+//     experience2.image = 'url'
+//     await experience2.save()
 
-    let destination = Destination.init()
-    destination.id = '1'
-    destination.name = 'Auckland Zoo'
-    destination.image = 'url'
-    destination.city = city.key
-    await destination.save()
+//     let destination = Destination.init()
+//     destination.id = '1'
+//     destination.name = 'Auckland Zoo'
+//     destination.image = 'url'
+//     destination.city = city.key
+//     await destination.save()
 
-    let destination2 = Destination.init()
-    destination2.id = '2'
-    destination2.name = 'Mission Bay'
-    destination2.image = 'url'
-    destination2.city = city2.key
-    await destination2.save()
+//     let destination2 = Destination.init()
+//     destination2.id = '2'
+//     destination2.name = 'Mission Bay'
+//     destination2.image = 'url'
+//     destination2.city = city2.key
+//     await destination2.save()
 
-    let tag = Tag.init()
-    tag.id = '1'
-    tag.name = 'National Parks'
-    tag.image = 'url'
-    await tag.save()
+//     let tag = Tag.init()
+//     tag.id = '1'
+//     tag.name = 'National Parks'
+//     tag.image = 'url'
+//     await tag.save()
 
-    let tag2 = Tag.init()
-    tag2.id = '2'
-    tag2.name = 'Beaches'
-    tag2.image = 'url'
-    await tag2.save()
-}
+//     let tag2 = Tag.init()
+//     tag2.id = '2'
+//     tag2.name = 'Beaches'
+//     tag2.image = 'url'
+//     await tag2.save()
+// }
 
 // See: https://www.apollographql.com/docs/apollo-server/integrations/middleware/#applying-middleware
 async function startApolloServer() {
@@ -110,5 +110,4 @@ async function startApolloServer() {
     return { server, app }
 }
 
-loadData()
 startApolloServer()
